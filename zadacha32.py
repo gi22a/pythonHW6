f9735bf00ce97eb9329919c18c17d5a2ec4ee5f4
Gizza <130634248+gi22a@users.noreply.github.com>
2023-08-15 09:25:53 +0300

    Add files via upload

---
+++ zadacha32.py
@@ -0,0 +1,28 @@
+# Задача 32: Определить индексы элементов массива (списка), 
+# значения которых принадлежат заданному диапазону 
+# (т.е. не меньше заданного минимума и не больше заданного максимума)
+
+import random
+
+min = int(input('Введите минимальное значение элемента \n'))
+max = int(input('Введите максимальное значение элемент \n'))
+n = int(input('Введите длину списка \n'))
+rand_start = int(input('Ведите начала диапазона random \n'))
+rand_end = int(input('Введите конец диапазона random \n'))
+
+
+list = [random.randint(rand_start, rand_end) for i in range(n)]   # Генерация списка вариант 1
+# list = [i for i in range(n)]                                     # Генерация списка вариант 2
+list_res = []
+
+def i_min_max(list):
+    for i in range(n):
+        if list[i] >=  min and list[i] <= max:
+            list_res.append(i)
+    return list_res
+
+print(list)
+print(i_min_max(list))
+
+for i in range(len(list_res)):
+    print(list[list_res[i]], end =", ")
